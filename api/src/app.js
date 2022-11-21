0346f1781a6941f29c6594b7078a6b55374984a1
Andreh-Z <witecan931@gmail.com>
2022-11-20 22:47:43 -0300

    clean

--- api/src/app.js
+++ api/src/app.js
@@ -29,7 +29,6 @@ server.use("/", routes);
 
 // Error catching endware.
 server.use((err, req, res, next) => {
-  // eslint-disable-line no-unused-vars
   const status = err.status || 500;
   const message = err.message || err;
   console.error(err);
