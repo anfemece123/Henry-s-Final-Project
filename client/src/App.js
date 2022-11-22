6b6fe277d50015ba18c439fff15a1b77d94e2b10
Dante1704 <dante.nkaddarian@gmail.com>
2022-11-22 15:54:08 -0300

    index modified

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,5 @@
-
-
 function App() {
-<<<<<<< HEAD
   return <div className="App">App Component</div>;
-=======
-  return <div className="App"> 
-  
-  <p> Modificacion de Andres </p>
-  </div>;
-
->>>>>>> 1b88d1f2251d03bedae8e0b450802bdc38d518c5
 }
 
 export default App;
