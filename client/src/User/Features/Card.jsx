9a33390b9a5024de28e476e6a0c9084fc672bc57
Andreh-Z <witecan931@gmail.com>
2022-11-23 19:25:47 -0300

    card_Home

--- client/src/User/Features/Card.jsx
+++ client/src/User/Features/Card.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
-export default function Card() {
-  return <div>Card</div>;
+export default function Card({ id, title, color, price, size, gender, stock }) {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{color}</p>
+      <p>{price}</p>
+      <p>{gender}</p>
+      <p>{size}</p>
+      <p>{stock}</p>
+    </div>
+  );
 }
