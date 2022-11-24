71cdb2362c0f7b8945c8d5fe51e773401beff4a3
Andreh-Z <witecan931@gmail.com>
2022-11-24 00:29:31 -0300

    agregados_productos_estilos/home/cards

--- client/src/User/Features/Card.jsx
+++ client/src/User/Features/Card.jsx
@@ -15,24 +15,34 @@ export default function Card({
 }) {
   return (
     <div className="border border-slate-900 max-w-sm rounded overflow-hidden shadow-lg">
-      <img className="w-full" src={images} />
+      <img
+        className="m-auto"
+        src={images}
+        style={{ width: "300px", height: "300px" }}
+      />
       <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">
+        <div className="font-bold text-xl text-center mb-2">
           <Link to={`/detail/${id}`}>
             <h1>{title}</h1>
           </Link>
         </div>
       </div>
-      <div className="w-1/2">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-          $ {price}
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-          Stock: {stock}
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-          Category: {category}
-        </span>
+      <div className="flex m-2 text-center">
+        <div>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            $ {price}
+          </span>
+        </div>
+        <div>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            Stock: {stock}
+          </span>
+        </div>
+        <div>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            Category: {category}
+          </span>
+        </div>
       </div>
     </div>
   );
