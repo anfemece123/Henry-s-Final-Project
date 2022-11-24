4f02fb3129b78691d8705f8b32488034e7f92bb4
Andreh-Z <witecan931@gmail.com>
2022-11-24 20:27:19 -0300

    filtro_generos

--- client/src/Redux/Store/index.js
+++ client/src/Redux/Store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "../Reducer/allProductSlice";
 import productDetails from "../Reducer/productDetails";
+import genderReducer from "../Reducer/filtersReducer";
 export default configureStore({
   reducer: {
     allProducts: productReducer,
     details: productDetails,
+    filterGender: genderReducer,
   },
 });
