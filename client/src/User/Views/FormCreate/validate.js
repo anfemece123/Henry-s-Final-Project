29bec5342a70a6a61e7362ae6741a0847211c341
Andreh-Z <witecan931@gmail.com>
2022-11-23 21:22:56 -0300

    develop

--- client/src/User/Views/FormCreate/validate.js
+++ client/src/User/Views/FormCreate/validate.js
@@ -1,94 +1,89 @@
-export default  function validate (values){
-    let errors= {}
-    let RegExpressionText = /^[a-zA-Z\s]*$/;  
-    let RegExpressionNum= /^[0-9,$]*$/
-    let RegExpressionUrl= /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/
+export default function validate(values) {
+  let errors = {};
+  let RegExpressionText = /^[a-zA-Z\s]*$/;
+  let RegExpressionNum = /^[0-9,$]*$/;
+  let RegExpressionUrl =
+    /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/;
 
-    //title
+  //title
 
-    if(!values.title){
-        errors.title= 'Por favor ingresa un title'
-    }
-    if(!RegExpressionText.test(values.name)){
-        errors.title = 'No se permiten números ni caracteres especiales'
-    }
+  if (!values.title) {
+    errors.title = "Por favor ingresa un title";
+  }
+  if (!RegExpressionText.test(values.name)) {
+    errors.title = "No se permiten números ni caracteres especiales";
+  }
 
-    //category
+  //category
 
-    if(!values.category){
-        errors.category= 'Por favor ingresa una category'
-    }
-    if(!RegExpressionText.test(values.category)){
-        errors.category = 'No se permiten números ni caracteres especiales'
-    }
+  if (!values.category) {
+    errors.category = "Por favor ingresa una category";
+  }
+  if (!RegExpressionText.test(values.category)) {
+    errors.category = "No se permiten números ni caracteres especiales";
+  }
 
-    //color
-    if(!values.color){
-        errors.color= 'Por favor ingresa un color'
-    }
-    if(!RegExpressionText.test(values.color)){
-        errors.color = 'No se permiten números ni caracteres especiales'
-    }
+  //color
+  if (!values.color) {
+    errors.color = "Por favor ingresa un color";
+  }
+  if (!RegExpressionText.test(values.color)) {
+    errors.color = "No se permiten números ni caracteres especiales";
+  }
 
-    //season
+  //season
 
-    if(!values.season){
-        errors.season= 'Por favor ingresa un season'
-    }
-    if(!RegExpressionNum.test(values.season)){
-        errors.season = 'Solo se permiten numeros'
-    }
+  if (!values.season) {
+    errors.season = "Por favor ingresa un season";
+  }
+  if (!RegExpressionNum.test(values.season)) {
+    errors.season = "Solo se permiten numeros";
+  }
 
-    //price
+  //price
 
-    if(!values.price){
-        errors.price= 'Por favor ingresa un price'
-    }
-    if(!RegExpressionNum.test(values.price)){
-        errors.price = 'Solo se permiten numeros'
-    }
+  if (!values.price) {
+    errors.price = "Por favor ingresa un price";
+  }
+  if (!RegExpressionNum.test(values.price)) {
+    errors.price = "Solo se permiten numeros";
+  }
 
-     //size
+  //size
 
-     if(!values.size){
-        errors.size= 'Por favor ingresa un size'
-    }
-    if(!RegExpressionText.test(values.size)){
-        errors.size = 'No se permiten números ni caracteres especiales'
-    }
+  if (!values.size) {
+    errors.size = "Por favor ingresa un size";
+  }
+  if (!RegExpressionText.test(values.size)) {
+    errors.size = "No se permiten números ni caracteres especiales";
+  }
 
-    //gender
+  //gender
 
-    if(!values.gender){
-        errors.gender= 'Por favor ingresa un gender'
-    }
-    if(!RegExpressionText.test(values.gender)){
-        errors.gender = 'No se permiten números ni caracteres especiales'
-    }
+  if (!values.gender) {
+    errors.gender = "Por favor ingresa un gender";
+  }
+  if (!RegExpressionText.test(values.gender)) {
+    errors.gender = "No se permiten números ni caracteres especiales";
+  }
 
-    //stock
-    if(!values.stock){
-        errors.stock= 'Por favor ingresa un stock'
-    }
-    if(!RegExpressionNum.test(values.stock)){
-        errors.stock = 'Solo se permiten numeros'
-    }
+  //stock
+  if (!values.stock) {
+    errors.stock = "Por favor ingresa un stock";
+  }
+  if (!RegExpressionNum.test(values.stock)) {
+    errors.stock = "Solo se permiten numeros";
+  }
 
-    //image
-    
-    if(!values.image){
-        errors.image= 'Por favor ingresa un image'
-    }
+  //image
 
-    if(!RegExpressionUrl.test(values.image)){
-        errors.image = 'Url incorrecto'
-    }
+  if (!values.image) {
+    errors.image = "Por favor ingresa un image";
+  }
 
-    
+  if (!RegExpressionUrl.test(values.image)) {
+    errors.image = "Url incorrecto";
+  }
 
-
-
-   
-    return errors
-
+  return errors;
 }
