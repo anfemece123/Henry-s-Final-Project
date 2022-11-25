95b198c14690b3d0b272c688afee2f38fad6636b
Dante1704 <dante.nkaddarian@gmail.com>
2022-11-25 19:16:00 -0300

    busqueda por rango de precios, cargado archivo endpoints a la carpeta general

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -7,6 +7,7 @@ const getByCategory = require("../controllers/product/product.byCategory.control
 const getByTitle = require("../controllers/product/product.byName.controller");
 const getByGender = require("../controllers/product/product.byGender.controller");
 const getByPriceOrder = require("../controllers/product/product.byPriceOrder.controller");
+const getByPriceRange = require("../controllers/product/product.byPriceRange.controller");
 
 const router = express.Router();
 
@@ -16,6 +17,7 @@ router.post("/createProduct", createNewProducts);
 router.get("/search", getByTitle);
 router.get("/byCategory", getByCategory);
 router.get("/byGender", getByGender);
+router.get("/byPriceRange", getByPriceRange);
 router.get("/:id", getDetails);
 
 module.exports = router;
