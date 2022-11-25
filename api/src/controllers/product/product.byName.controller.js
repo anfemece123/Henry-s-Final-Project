ac2133253cc876d67835ada2fdceebdcff2d4ac6
Dante1704 <dante.nkaddarian@gmail.com>
2022-11-25 16:14:17 -0300

    implementado si en la searchbar no se envia ningun nombre, devuelve todos productos

--- api/src/controllers/product/product.byName.controller.js
+++ api/src/controllers/product/product.byName.controller.js
@@ -1,18 +1,24 @@
-//product controllers
-const express = require("express");
-const axios = require("axios");
-
 const { Product } = require("../../db");
 
 getByTitle = async (req, res) => {
+  const { title } = req.query;
+  if (!title) {
+    try {
+      const allProducts = await Product.findAll();
+      return res.status(200).send(allProducts);
+    } catch (error) {
+      return res.status(404).send(error.message);
+    }
+  }
   try {
-    const {title}=req.query;
-    let products = await Product.findAll();
-    let searched=products.filter(element=>element.title.toLowerCase().includes(title.toLowerCase()));
-    if (!searched.length) return res.status(400).send("Product Not Found");
-      return res.status(200).send(searched);
+    const allProducts = await Product.findAll();
+    let found = allProducts.filter((element) =>
+      element.title.toLowerCase().includes(title.toLowerCase())
+    );
+    if (!found.length) return res.status(400).send("Product Not Found");
+    return res.status(200).send(found);
   } catch (error) {
-    return res.status(404).send(error);
+    return res.status(404).send(error.message);
   }
 };
 
