8dfb001d53ed7e50dff04e39a1d457f09882f636
anfemece123 <90008499+anfemece123@users.noreply.github.com>
2022-11-26 11:05:20 -0500

    mejoras del searchBar

---
+++ client/src/User/Features/ErrorSearch.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import NavBar from "./NavBar";
+
+export const ErrorSearch = () => {
+  return (
+    <div>
+      <NavBar />
+
+      <img
+        width="500px"
+        src="https://cdn.dribbble.com/users/3512533/screenshots/14168376/media/1357b33cb4057ecb3c6f869fc977561d.jpg"
+      />
+    </div>
+  );
+};
