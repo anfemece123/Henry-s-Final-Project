247677358994bdb585a382e8643e12f697a9961f
Andreh-Z <witecan931@gmail.com>
2022-11-29 19:34:26 -0300

    more fixes

--- client/src/Admin/Features/Sidebar/Sidebar.js
+++ client/src/Admin/Features/Sidebar/Sidebar.js
@@ -24,7 +24,7 @@ export default function Sidebar() {
             className="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0"
             to="/"
           >
-            Notus React
+            TIENDA NUESTRA
           </Link>
           {/* User */}
           <ul className="md:hidden items-center flex flex-wrap list-none">
