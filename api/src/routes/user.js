0266a3b707c6c8bf95d31017957124c80897660c
Dante1704 <dante.nkaddarian@gmail.com>
2022-11-30 11:12:53 -0300

    borrado logico del usuario (isBanned:true).

--- api/src/routes/user.js
+++ api/src/routes/user.js
@@ -6,13 +6,14 @@ const {
   updateUser,
   getUserDetail,
   getAllUsers,
+  deleteUser,
 } = require("../controllers/user/user.controller");
 
 const router = express.Router();
 
 router.post("/newUser", createNewUser);
-router.put("/update", /* userIdAndIsAdminExtractor , */ updateUser);
-router.get("/allUsers", /* userIdAndIsAdminExtractor , */ getAllUsers);
-router.get("/:id", /* userIdAndIsAdminExtractor , */ getUserDetail);
-
+router.put("/update", /* userIdAndIsAdminExtractor , */ updateUser); //only users
+router.get("/allUsers", /* userIdAndIsAdminExtractor , */ getAllUsers); ////only admin
+router.get("/:id", /* userIdAndIsAdminExtractor , */ getUserDetail); // users and admin
+router.put("/delete/:id" /* userIdAndIsAdminExtractor , */, deleteUser); //only admin
 module.exports = router;
