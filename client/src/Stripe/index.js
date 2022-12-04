cc4bf38f25ddf01ec21a114737046c5d55ea952a
Andreh-Z <witecan931@gmail.com>
2022-12-03 21:44:42 -0300

    asdas

--- client/src/Stripe/index.js
+++
@@ -1,7 +0,0 @@
-import { loadStripe } from "@stripe/stripe-js";
-
-const stripePromise = loadStripe(
-  "pk_test_51MA0JJA6JLprqSfEVmSQNdhfLDRKUiSFM80ExAXTHYM4wrINoIcFXtdLuVVMukT947zFINvYnCA6QPNzBXNEMxvV00PiQs1aKn"
-);
-
-export default stripePromise;
