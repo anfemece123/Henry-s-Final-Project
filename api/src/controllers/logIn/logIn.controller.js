58e77c30effa4111cece988eb1a4b221428c5c80
Dante1704 <dante.nkaddarian@gmail.com>
2022-12-09 19:54:57 -0300

    login google para arreglar

--- api/src/controllers/logIn/logIn.controller.js
+++ api/src/controllers/logIn/logIn.controller.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const jwt_decode = require("jwt-decode");
 const { User } = require("../../db");
 const { Cart } = require("../../db");
 const { TOKEN_SECRET } = process.env;
@@ -59,77 +60,77 @@ logInUser = async (req, res) => {
       isAdmin: userAux.isAdmin,
       token,
     };
-    const UserId = userAux.id;
-    const cart = await Cart.findOne({ where: { UserId } });
+    const id = userAux.id;
+    const cart = await Cart.findOne({ where: { id } });
     if (cart) console.log("cart:", cart.dataValues);
 
     return res.status(200).send([loggedUser, cart]);
   } catch (error) {
-    console.log(error);
     return res.status(404).send(error);
   }
 };
 
 logInGoogle = async (req, res) => {
   const { credentials } = req.body;
-  console.log("credentials: ", credentials);
+
   // valido todos los datos recibidos
-  /*  if (!email || !password) {
-    return res.status(400).send("Missing Data");
+  if (!credentials) {
+    return res.status(400).send("Wrong Credentials");
   }
-  //aca ya tengo todos los datos
-  //me fijo si existe el usuario
+  const userAux = jwt_decode(credentials);
+  const password = userAux.jti;
+  const passwordHashed = await bcrypt.hash(password, 10 /* saltRounds */);
+  console.log("userAux", userAux);
+
   try {
-    //solo chequeo el email porque es unique, con eso me alcanza
-    const userAux = await User.findOne({
+    const email = userAux.email;
+    const [googleUser, created] = await User.findOrCreate({
       where: { email },
+      defaults: {
+        first_name: userAux.given_name,
+        last_name: userAux.family_name,
+        email: userAux.email,
+        passwordHashed,
+        phoneNumber: "Not Found",
+        address: "Not Found",
+        profileImage: userAux.picture,
+        isAdmin: password === process.env.PASS_ADMIN_SECRET || false, //si me me envian el password correcto se setea como admin sino siempre false
+        isBanned: false,
+        status: "active",
+        confirmationCode: userAux.jti,
+      },
     });
-    if (!userAux) return res.status(401).send("Username Or Password Wrong");
-    //aca tengo usuario, solo me falta verificar la contraseña
-    const passwordCorrect = await bcrypt.compare(
-      password,
-      userAux.passwordHashed
-    );
-    if (!passwordCorrect)
-      return res.status(401).send("Username Or Password Wrong");
-    if (userAux.isBanned)
-      return res
-        .status(403)
-        .send("Your Account Is Banned, Contact With The Company");
-    if (userAux.status != "active") {
-      return res.status(401).send("Pending Account. Please Verify Your Email!");
-    }
-    //aca tengo email y password correctos
+
     const userForToken = {
-      id: userAux.id,
-      email,
+      id: googleUser.id,
+      email: googleUser.email,
       password,
-      isAdmin: userAux.isAdmin,
+      isAdmin: googleUser.isAdmin,
     };
+
     const token = jwt.sign(userForToken, TOKEN_SECRET, {
       expiresIn: 60 * 60 * 24 * 30, //que se tenga que loguear cada 30 dias
     });
     const loggedUser = {
-      id: userAux.id,
-      first_name: userAux.first_name,
-      last_name: userAux.last_name,
-      address: userAux.address,
-      email: userAux.email,
-      phoneNumber: userAux.phoneNumber,
-      address: userAux.address,
-      profileImage: userAux.profileImage,
-      isAdmin: userAux.isAdmin,
+      id: googleUser.id,
+      first_name: googleUser.first_name,
+      last_name: googleUser.last_name,
+      address: googleUser.address,
+      email: googleUser.email,
+      phoneNumber: googleUser.phoneNumber,
+      address: googleUser.address,
+      profileImage: googleUser.profileImage,
+      isAdmin: googleUser.isAdmin,
       token,
     };
-    const UserId = userAux.id;
-    const cart = await Cart.findOne({ where: { UserId } });
-    if (cart) console.log("cart:", cart.dataValues);
-
+    const id = googleUser.id;
+    const cart = await Cart.findOne({ where: { id } });
+    console.log("loggedUser: ", loggedUser);
+    console.log("cart: ", cart);
     return res.status(200).send([loggedUser, cart]);
   } catch (error) {
-    console.log(error);
     return res.status(404).send(error);
-  } */
+  }
 };
 
 module.exports = { logInUser, logInGoogle };
