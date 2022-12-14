9559eea14cadc37cd03352841663eed87a0b20fc
anfemece123 <90008499+anfemece123@users.noreply.github.com>
2022-12-14 18:33:30 -0500

    arreglos

--- client/src/User/Features/Footer.jsx
+++ client/src/User/Features/Footer.jsx
@@ -8,18 +8,19 @@ import LockOpenIcon from "@mui/icons-material/LockOpen";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import PersonSearchIcon from "@mui/icons-material/PersonSearch";
+import logo from "../../Images/image.png";
 
 export default function Footer() {
   return (
     <footer className=" inset-x-0 bottom-0 p-5 bg-gray-900 sm:p-6 dark:bg-gray-900">
       <div className="md:flex md:justify-between">
-        <div className="mb-6 md:mb-0">
-          <a href="asdas" className="flex items-center">
-            {/* <img src="asdasd" className="mr-3 h-8" alt="Icono nuestro" /> */}
-            <span className="self-center text-2xl font-cursive-titles font-semibold whitespace-nowrap text-white">
+        <div className="mb-1 md:mb-0">
+          {/* <a href="asdas" className="flex items-center"> */}
+          <img src={logo} className="mr-3 h-20" alt="Icono nuestro" />
+          {/* <span className="self-center text-2xl font-cursive-titles font-semibold whitespace-nowrap text-white">
               Tienda Nuestra
             </span>
-          </a>
+          </a> */}
         </div>
         <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
           <div>
