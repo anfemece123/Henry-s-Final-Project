d50a3bc0a7ef2975f669793012f14a3e5e09fcdb
Andreh-Z <witecan931@gmail.com>
2022-12-14 14:53:06 -0300

    footer arreglado/aboutUs/landing

--- client/src/User/Features/Footer.jsx
+++ client/src/User/Features/Footer.jsx
@@ -1,4 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import EmailIcon from "@mui/icons-material/Email";
+import GpsFixedIcon from "@mui/icons-material/GpsFixed";
+import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
+import HowToRegIcon from "@mui/icons-material/HowToReg";
+import LockOpenIcon from "@mui/icons-material/LockOpen";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import PersonSearchIcon from "@mui/icons-material/PersonSearch";
 
 export default function Footer() {
   return (
@@ -14,53 +23,61 @@ export default function Footer() {
         </div>
         <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
           <div>
-            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-              Links
+            <h2 className="mb-2 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+              shortcuts
             </h2>
             <ul className="text-gray-600 dark:text-gray-400">
-              <li className="mb-4">
-                <a href="" className="hover:underline">
-                  Fashion A
-                </a>
+              <li className="mb-2">
+                <Link to="/formRegister" className="hover:underline">
+                  <HowToRegIcon className="m-2" /> Register
+                </Link>
               </li>
-              <li>
-                <a href="" className="hover:underline">
-                  Fashion B
-                </a>
+              <li className="mb-2">
+                <Link to="/login" className="hover:underline">
+                  <LockOpenIcon className="m-2" /> Log In
+                </Link>
+              </li>
+              <li className="mb-2">
+                <Link to="/cart" className="hover:underline">
+                  <ShoppingCartIcon className="m-2" /> Cart
+                </Link>
               </li>
             </ul>
           </div>
           <div>
-            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-              CONTACT
+            <h2 className="mb-2 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+              PROJECT and TEAM
             </h2>
             <ul className="text-gray-600 dark:text-gray-400">
               <li className="mb-4">
-                <a href="https://github.com/" className="hover:underline ">
-                  Github
+                <a
+                  href="https://github.com/anfemece123/Henry-s-Final-Project"
+                  className="hover:underline "
+                >
+                  <GitHubIcon className="m-2" /> GitHub project
                 </a>
               </li>
               <li>
-                <a href="https://discord" className="hover:underline">
-                  Discord
-                </a>
+                <Link to="/aboutUs" className="hover:underline">
+                  <PersonSearchIcon className="m-2" /> Developers
+                </Link>
               </li>
             </ul>
           </div>
           <div>
-            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-              FAQ
+            <h2 className="mb-2 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+              Find Us
             </h2>
             <ul className="text-gray-600 dark:text-gray-400">
-              <li className="mb-4">
-                <a href="#" className="hover:underline">
-                  About Us
-                </a>
+              <li className="mb-2">
+                <GpsFixedIcon className="m-2" /> Nebraska - 208 S Broad St
               </li>
-              <li>
-                <a href="#" className="hover:underline">
-                  Terms & Conditions
-                </a>
+              <li className="mb-2">
+                <EmailIcon className="m-2" /> TiendaNuestra@atencion.com
+              </li>
+              <li className="mb-2">
+                <LocalPhoneIcon className="m-2" />
+                (252) 482-2676
               </li>
             </ul>
           </div>
