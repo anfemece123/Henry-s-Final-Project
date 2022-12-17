cf1af35bb2ce4852febcdd61799cf56c11e7f921
Dante1704 <dante.nkaddarian@gmail.com>
2022-12-16 23:11:03 -0300

    Refix:ocultado el componente de las reviews cuando el producto tiene reviews no visibles

--- client/src/User/Views/Rating/ReviewSystem.jsx
+++ client/src/User/Views/Rating/ReviewSystem.jsx
@@ -8,30 +8,32 @@ export default function ReviewSystem(props) {
   return (
     <div>
       {details.Reviews &&
-        details.Reviews.every((review) => review.isVisible === true) &&
-        details.Reviews.map((review) => (
-          <div className="border border-slate-600 shadow-2xl shadow-gray-600 hover:shadow-2xl">
-            <p>Product Reviews</p>
+        details.Reviews.map(
+          (review) =>
+            review.isVisible && (
+              <div className="border border-slate-600 shadow-2xl shadow-gray-600 hover:shadow-2xl">
+                <p>Product Reviews</p>
 
-            <div>
-              {review.isVisible && (
                 <div>
-                  <p>User:{review.UserId}</p>
-                  <p>
-                    Calification:
-                    <Rating
-                      name="half-rating-read"
-                      defaultValue={review.calification}
-                      precision={0.5}
-                      readOnly
-                    />
-                  </p>
-                  <p>Comment:{review.comment}</p>
+                  {review.isVisible && (
+                    <div>
+                      <p>User:{review.UserId}</p>
+                      <p>
+                        Calification:
+                        <Rating
+                          name="half-rating-read"
+                          defaultValue={review.calification}
+                          precision={0.5}
+                          readOnly
+                        />
+                      </p>
+                      <p>Comment:{review.comment}</p>
+                    </div>
+                  )}
                 </div>
-              )}
-            </div>
-          </div>
-        ))}
+              </div>
+            )
+        )}
     </div>
   );
 }
