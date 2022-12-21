5ec8832fbca49223ce532ff4fe2d9edef53e54cd
Dante1704 <dante.nkaddarian@gmail.com>
2022-12-21 16:45:17 -0300

    landing terminada. Footer modificado

--- client/src/User/Views/Landing.jsx
+++ client/src/User/Views/Landing.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { MDBContainer, MDBRow, MDBrow } from "mdb-react-ui-kit";
+import { MDBContainer, MDBRow, MDBCol, MDBBtn } from "mdb-react-ui-kit";
 import { Container, Row, Col } from "react-bootstrap";
 import NavBar from "../Features/NavBar";
 import girLanding from "../../Images/bg2.png";
@@ -10,78 +10,61 @@ import logo from "../../Images/logo.png";
 
 export default function Landing() {
   return (
-    <Container
-      className="grid"
-      style={{
-        margin: 0,
-        padding: 0,
-      }}
-    >
-      <Row
-        className=""
-        style={{
-          margin: 0,
-        }}
-      >
-        <Col
+    <MDBContainer fluid className="mx-0">
+      <MDBRow className="">
+        <MDBCol
           style={{
-            margin: 0,
+            padding: 0,
           }}
         >
-          <NavBar />
-        </Col>
-      </Row>
-      <Row
-        className="bg-image img-fluid"
-        style={{
-          backgroundImage:
-            "url('https://res.cloudinary.com/dyfjoi0td/image/upload/v1671049872/ecommerce/pexels-ylanite-koppens-934064_6_rc0trb.jpg')",
-          margin: 0,
-        }}
-      >
-        <Col
-          style={{
-            margin: 0,
-          }}
-        >
-          <img src={logo} className="" />
-          <div className="">
-            <div className="">
-              <h1>The style, is your personality</h1>
-            </div>
-            <div className="">
-              <button className="">
-                <Link to="/home">WATCH CATALOG</Link>
-              </button>
-            </div>
-          </div>
-        </Col>
-      </Row>
-      {/*       <Row className="">
-        <Footer />
-      </Row> */}
-    </Container>
+          <MDBRow>
+            <NavBar />
+          </MDBRow>
+          <MDBRow
+            style={{
+              backgroundImage:
+                "url('https://res.cloudinary.com/dyfjoi0td/image/upload/v1671049872/ecommerce/pexels-ylanite-koppens-934064_6_rc0trb.jpg')",
+              margin: 0,
+              backgroundPosition: "center",
+              backgroundRepeat: "no - repeat",
+              backgroundSize: "cover",
+              minHeight: "661px",
+              alignItems: "center",
+              paddingLeft: "5%",
+            }}
+          >
+            <MDBCol className="d-flex flex-column mb-3">
+              <img
+                src={logo}
+                className="mb-3"
+                style={{ minWidth: "200px", maxWidth: "30vw", padding: 0 }}
+              />
+              <h1
+                className="mb-3"
+                style={{ marginBottom: 0, textAlign: "left", padding: 0 }}
+              >
+                The style, is your personality
+              </h1>
+              <MDBBtn
+                className="mb-3"
+                style={{ maxHeight: "100px", maxWidth: "180px" }}
+              >
+                <Link
+                  style={{ textDecoration: "none", color: "white" }}
+                  to="/home"
+                >
+                  WATCH CATALOG
+                </Link>
+              </MDBBtn>
+            </MDBCol>
+          </MDBRow>
+          <MDBRow className="">
+            <MDBCol>
+              <Footer />
+            </MDBCol>
+          </MDBRow>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
   );
-}
-
-{
-  /* <Container fluid="lg" className="border border-2">
-  <Row>
-    <row lg={12}>row</row>
-    <row lg={6} md={6}>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim, totam
-      assumenda rerum in est inventore quam fuga atque, eum perferendis nam
-      molestias debitis fugit? Praesentium officiis voluptate enim alias neque.
-    </row>
-    <row lg={6} md={6}>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. A pariatur
-      perferendis molestias est laudantium eaque sapiente. Nihil soluta
-      deserunt, facere dolor aliquid in, culpa voluptas temporibus sed
-      dignissimos repellendus nemo?
-    </row>
-    <row lg={8}>row</row>
-    <row lg={10}>row</row>
-    <row>row</row>
-  </Row>
-</Container>; */
 }
