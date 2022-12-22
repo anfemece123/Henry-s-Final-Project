ab1f8a538b87c1a253641e1233a9c9dcf1ccef83
anfemece123 <90008499+anfemece123@users.noreply.github.com>
2022-12-22 15:20:31 -0500

    panel Admin

--- client/src/Admin/Views/Components/ReviewsAdmin.js
+++ client/src/Admin/Views/Components/ReviewsAdmin.js
@@ -1,7 +1,13 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-
+import {
+  MDBBadge,
+  MDBBtn,
+  MDBTable,
+  MDBTableHead,
+  MDBTableBody,
+} from "mdb-react-ui-kit";
 import Rating from "@mui/material/Rating";
 import BlockIcon from "@mui/icons-material/Block";
 import {
@@ -25,117 +31,94 @@ export default function ReviewsAdmin() {
   }
 
   return (
-    <>
-      <div className=" relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
-        <div className="rounded-t mb-0 px-4 py-3 border-0">
-          <div className="flex flex-wrap items-center">
-            <div className="relative w-full px-4 max-w-full flex-grow flex-1">
-              <h3 className="pt-40 pb-10 text-2xl font-bold tracking-wide">
-                Reviews
-              </h3>
-            </div>
-          </div>
-        </div>
-        <div className="block w-full overflow-x-auto">
-          {/* Projects table */}
-          <table className="items-center w-full bg-transparent border-collapse">
-            <thead className="thead-light">
-              <tr>
-                <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-                  USER NAME
-                </th>
-                <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-                  PRODUCT
-                </th>
-                <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-                  CALIFICATION
-                </th>
-                <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left min-w-140-px">
-                  COMENT
-                </th>
-                <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left min-w-140-px">
-                  VISIBLE
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {Array.isArray(reviews) ? (
-                reviews.map((element) => {
-                  return (
-                    <tr key={element.comment}>
-                      <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
-                        {element.User.first_name} {element.User.last_name}
-                      </th>
-                      <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
-                        {element.Product.title}{" "}
-                        <img
-                          src={element.Product.image}
-                          className="h-12 w-12 bg-white rounded-full border"
-                          alt="..."
-                        ></img>
-                      </th>
-                      <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
-                        <Rating
-                          name="half-rating-read"
-                          defaultValue={element.calification}
-                          precision={0.5}
-                          readOnly
-                        />
-                      </th>
-                      <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                        {element.comment}
-                      </td>
-                      <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                        {element.isVisible ? "yes" : "No"}
-                        <button
-                          value={element.id}
-                          type="submit"
-                          onClick={() => {
-                            {
-                              alertButtonDelete(element.id);
-                            }
-                          }}
-                        >
-                          <div>
-                            <a className="decoration-red-500 mr-2 text-rose-600">
-                              Ban
-                            </a>
-                            <BlockIcon color="warning" />
-                          </div>
-                        </button>
-                      </td>
-
-                      <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4"></td>
-                    </tr>
-                  );
-                })
-              ) : (
+    <MDBTable responsive>
+      <MDBTableHead>
+        <tr className="table-warning">
+          <th scope="col">User</th>
+          <th scope="col">Product</th>
+          <th scope="col">Calification</th>
+          <th scope="col">Coments</th>
+          <th scope="col">Visible</th>
+          <th scope="col">Actions</th>
+        </tr>
+      </MDBTableHead>
+      <MDBTableBody>
+        {Array.isArray(reviews)
+          ? reviews.map((element) => {
+              return (
                 <tr>
-                  <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
-                    Facebook
-                  </th>
-                  <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                    1,480
-                  </td>
-                  <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                    <div className="flex items-center">
-                      <span className="mr-2">60%</span>
-                      <div className="relative w-full">
-                        <div className="overflow-hidden h-2 text-xs flex rounded bg-red-200">
-                          <div
-                            style={{ width: "60%" }}
-                            className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-red-500"
-                          ></div>
-                        </div>
+                  <td>
+                    <div className="d-flex align-items-center">
+                      <img
+                        src={element.User.profileImage}
+                        alt=""
+                        style={{ width: "45px", height: "45px" }}
+                        className="rounded-circle"
+                      />
+                      <div className="ms-3">
+                        <p className="fw-bold mb-1">
+                          {element.User.first_name} {element.User.last_name}
+                        </p>
                       </div>
                     </div>
                   </td>
+                  <td>
+                    <div className="d-flex align-items-center">
+                      <img
+                        src={element.Product.image}
+                        alt=""
+                        style={{ width: "45px", height: "45px" }}
+                        className="rounded-circle"
+                      />
+                      <div className="ms-3">
+                        <p className="fw-bold mb-1">{element.Product.title}</p>
+                      </div>
+                    </div>
+                  </td>
+                  <td>
+                    <Rating
+                      name="half-rating-read"
+                      defaultValue={element.calification}
+                      precision={0.5}
+                      readOnly
+                    />
+                  </td>
+                  <td>
+                    <p>{element.comment}</p>
+                  </td>
+                  <td>
+                    {element.isVisible ? (
+                      <MDBBadge color="success" pill>
+                        {element.isVisible ? "Active" : "Baned"}
+                      </MDBBadge>
+                    ) : (
+                      <MDBBadge color="danger" pill>
+                        {element.isVisible ? "Active" : "Baned"}
+                      </MDBBadge>
+                    )}
+                  </td>
+
+                  <td>
+                    <MDBBtn
+                      value={element.id}
+                      type="submit"
+                      onClick={() => {
+                        {
+                          alertButtonDelete(element.id);
+                        }
+                      }}
+                      color="link"
+                      rounded
+                      size="sm"
+                    >
+                      Ban
+                    </MDBBtn>
+                  </td>
                 </tr>
-              )}
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </>
+              );
+            })
+          : null}
+      </MDBTableBody>
+    </MDBTable>
   );
 }
--- client/src/Admin/Views/Components/UsersAdmin.js
+++ client/src/Admin/Views/Components/UsersAdmin.js
@@ -2,9 +2,13 @@ import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { deleteUserId, getAllusers } from "../../../Redux/Reducer/Users";
-import BlockIcon from "@mui/icons-material/Block";
-
-// components
+import {
+  MDBBadge,
+  MDBBtn,
+  MDBTable,
+  MDBTableHead,
+  MDBTableBody,
+} from "mdb-react-ui-kit";
 
 export default function UsersAdmin() {
   const user = useSelector((state) => state.users.allUsers);
@@ -22,125 +26,74 @@ export default function UsersAdmin() {
   if (useSelector((state) => !state.users.loading && state.users.error))
     return <h1>{error}</h1>;
   return (
-    <>
-      <h3 className="pt-40 pb-10 text-2xl font-bold tracking-wide ">Users</h3>
-      <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded ">
-        <div className="rounded-t mb-0 px-4 py-3 border-0">
-          <div className="flex flex-wrap items-center">
-            <div className="relative w-full px-4 max-w-full flex-grow flex-1"></div>
-          </div>
-        </div>
-        <div className="block w-full overflow-x-auto">
-          <table className="items-center w-full bg-transparent border-collapse">
-            <thead>
-              <tr>
-                <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-                  Profile image
-                </th>
-                <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-                  User Name
-                </th>
-
-                <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-                  Email
-                </th>
-                <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-                  Privileges(ADM)
-                </th>
-                <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-                  Banned
-                </th>
-                <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-                  phoneNumber
-                </th>
-                <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-                  Address
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {Array.isArray(user) ? (
-                user.map((element) => {
-                  return (
-                    <tr key={element.id}>
-                      <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
-                        <img
-                          src={element.profileImage}
-                          className="h-12 w-12 bg-white rounded-full border"
-                          alt="..."
-                        ></img>{" "}
-                      </th>
-                      <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
-                        {element.first_name} {element.last_name} (ID:{" "}
-                        {element.id})
-                      </th>
-                      <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                        {element.email}
-                      </td>
-                      <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                        {element.isAdmin ? "yes" : "no"}
-                      </td>
-
-                      <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                        {element.isBanned ? "yes" : "No"}
-                      </td>
-                      <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                        {element.phoneNumber}
-                      </td>
-                      <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                        {element.address}
-                      </td>
-
-                      <td className="border-t-0 px-6 align-middle border-l-0 border-r-0  whitespace-nowrap p-4">
-                        <button
-                          value={element.id}
-                          type="submit"
-                          onClick={() => {
-                            {
-                              alertButtonDelete(element.id);
-                            }
-                          }}
-                        >
-                          <div>
-                            <a className="decoration-red-500 mr-2 text-rose-600">
-                              Ban
-                            </a>
-                            <BlockIcon color="warning" />
-                          </div>
-                        </button>
-                      </td>
-                    </tr>
-                  );
-                })
-              ) : (
+    <MDBTable responsive>
+      <MDBTableHead>
+        <tr className="table-primary">
+          <th scope="col">Name</th>
+          <th scope="col">information</th>
+          <th scope="col">Banned</th>
+          <th scope="col">Privileges(ADM)</th>
+          <th scope="col">Actions</th>
+        </tr>
+      </MDBTableHead>
+      <MDBTableBody>
+        {Array.isArray(user)
+          ? user.map((element) => {
+              return (
                 <tr>
-                  <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
-                    {user.first_name}
-                  </th>
-                  <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                    {user.email}
+                  <td>
+                    <div className="d-flex align-items-center">
+                      <img
+                        src={element.profileImage}
+                        alt=""
+                        style={{ width: "45px", height: "45px" }}
+                        className="rounded-circle"
+                      />
+                      <div className="ms-3">
+                        <p className="fw-bold mb-1">
+                          {element.first_name} {element.last_name}
+                        </p>
+                        <p className="text-muted mb-0">{element.email}</p>
+                      </div>
+                    </div>
                   </td>
-                  <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                    {user.isAdmin ? "true" : "false"}
+                  <td>
+                    <p className="fw-normal mb-1">{element.address}</p>
+                    <p className="text-muted mb-0">{element.phoneNumber}</p>
                   </td>
-                  <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                    {user.isBanned ? "true" : "false"}
+                  <td>
+                    {element.isBanned ? (
+                      <MDBBadge color="danger" pill>
+                        {element.isBanned ? "Baned" : "Active"}
+                      </MDBBadge>
+                    ) : (
+                      <MDBBadge color="success" pill>
+                        {element.isBanned ? "Baned" : "Active"}
+                      </MDBBadge>
+                    )}
                   </td>
-                  {user.isBanned ? (
-                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                      <Button>UnBan</Button>
-                    </td>
-                  ) : (
-                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                      <button>Ban</button>
-                    </td>
-                  )}
+                  <td>{element.isAdmin ? "Yes" : "No"}</td>
+                  <td>
+                    <MDBBtn
+                      value={element.id}
+                      type="submit"
+                      onClick={() => {
+                        {
+                          alertButtonDelete(element.id);
+                        }
+                      }}
+                      color="link"
+                      rounded
+                      size="sm"
+                    >
+                      Ban
+                    </MDBBtn>
+                  </td>
                 </tr>
-              )}
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </>
+              );
+            })
+          : null}
+      </MDBTableBody>
+    </MDBTable>
   );
 }
